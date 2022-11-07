a929e2ea04d040a5e1b37ebb5565e25f67281dfe
leastbad <hello@leastbad.com>
2022-11-07 03:11:51 -0500

    first

---
+++ app/frontend/controllers/index.js
@@ -0,0 +1,7 @@
+import { application } from './application'
+import { registerControllers } from 'stimulus-vite-helpers'
+
+const controllers = import.meta.globEager('./**/*_controller.js')
+registerControllers(application, controllers)
+
+// application.load(controllers)
